c0c45f7c4d12554045bbaf70be39b2bc9d932ff2
Görkem <gorkem.kezr@gmail.com>
2025-03-01 14:41:13 +0300

    Login Testi Aşama1(Jiradaki gelen task'daki başlık)

---
+++ src/deneme.java
@@ -0,0 +1,12 @@
+public class deneme {
+    public static void main(String[] args) {
+
+
+        //bu bölüm login testi için yeni şifre modeline göre yazıldı
+        System.out.println("Yeni Login Testi");
+
+        //çok satır yazdım ve kodlarımı bitirdim
+        System.out.println("Login Testi2");
+
+    }
+}
